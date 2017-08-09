e34a5cc8170826b7b2a7f97675295c71e541c58d
KCW\KCWzzz <kcwzzz@naver.com>
2017-08-10 03:22:36 +0900

    no message

---
+++ 170810/Classes/CActorBullet.cpp
@@ -0,0 +1,46 @@
+#include "CActorBullet.h"
+#include "cocos2d.h"
+
+#include "GameScene.h"
+#include "CActor.h"
+#include "CEnemy.h"
+
+USING_NS_CC;
+
+CActorBullet::CActorBullet()
+{
+	Clear();
+}
+CActorBullet:: ~CActorBullet()
+{
+
+}
+
+void CActorBullet::Create()
+{
+	mpUnitImage = Sprite::create("CloseNormal.png");
+	mpUnitImage->retain();
+	mpUnitImage->setAnchorPoint(Vec2(0.5, 0));
+	mSpeedPower = 5.0f;
+	mDamage = 100.0f;
+}
+
+
+void CActorBullet::BulletMove(float dt)
+{
+		mVec.x = mVec.x;
+		mVec.y = mVec.y + mSpeedPower;
+		setPosition(mVec);
+}
+
+void CActorBullet::BulletDestroy()
+{
+	if (mVec.y > 800)
+	{
+		mIsLife = false;
+		mpScene->removeChild(mpUnitImage, 10);
+		setPosition(mVec);
+	}
+}
+
+
