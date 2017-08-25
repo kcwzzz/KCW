79bdc9029202974d94e3ea597b9eb32ba7afb048
kcwzzz <kcwzzz@naver.com>
2017-08-25 16:36:20 +0900

    no message

---
+++ Project 모음/170825/170825/Classes/CObjectAniBox.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "cocos2d.h"
+#include <iostream>
+
+using namespace cocos2d;
+using namespace std;
+
+
+class CObjectAniBox
+{
+protected:
+	Node *mpScene = NULL;
+	string mAniName = "";
+	float mDelayTime = 0.0f;
+	Vec2 mVec;
+	int mFrameCount = 0;
+
+
+	int mDir = 0;
+
+	Sprite *mpSprite = NULL;
+	Texture2D *mpTexture = NULL;
+
+	Animation *mpAnimationObject = NULL;
+	Animate *mpAnimateObject = NULL;
+
+	Animate *mpCurAnimate = NULL;
+
+public:
+
+	void SetScene(Node *tpNode);
+
+	void CreateAniBox(string tAniName, Vec2 tVec, int tWidth, int tHeight, float tSetDelay, int tFrameCount);
+
+	void CreateTexture(string tAniName, int tWidth, int tHeight);
+	void SetPosition(Vec2 tVec);
+
+	void CreateAniObject(int tWidth, int tHeight, float tSetDelay, int tFrameCount);
+	void RunAniObject();
+	void Build();
+
+	//void RotateUp();
+
+	void StopAnimation();
+
+	void Show();
+	void Hide();
+
+
+};
