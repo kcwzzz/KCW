e1e3727f79be2d5073d5f0ed52b8a5fe962fc958
kcwzzz <kcwzzz@naver.com>
2017-09-18 17:31:06 +0900

    no message

---
+++ Unity/Hero_2d_Unity/Assets/CTestArray.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CTestArray : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+        Debug.Log("@rectangular array");
+        int tRow = 0;
+        int tCol = 0;
+
+        int[,] tArray = new int[10, 10];
+
+        for (tRow = 0; tRow < tArray.GetLength(0); tRow++)
+        {
+            for (tCol = 0; tCol < tArray.GetLength(1); tCol++)
+            {
+                tArray[tRow, tCol] = tRow * 10 + tCol;
+            }
+        }
+
+        for (tRow = 0; tRow < tArray.GetLength(0); tRow++)
+        {
+            for (tCol = 0; tCol < tArray.GetLength(1); tCol++)
+            {
+                Debug.Log(tArray[tRow, tCol].ToString());
+            }
+        }        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Unity/Hero_2d_Unity/Assets/CUIBtnRCPGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CUIBtnRCPGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnclickBtnRSPGame()
+    {
+        Debug.Log("RSP GAme Start");        
+    }
+
+
+}
